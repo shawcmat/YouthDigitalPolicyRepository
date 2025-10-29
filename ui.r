55ffc8e19cd33da8ad62f3168dcec72046aee282
Matt Shawcroft <shawcroft.matt@gmail.com>
2025-10-29 12:20:23 -0400

    Change to menu bar layout

--- ui.r
+++ ui.r
@@ -1,25 +1,35 @@
-ui <- fluidPage(
-  includeCSS("www/styles.css"),
-  h2("US Media Policy Dataset Dashboard"),
-  br(),
-  sidebarLayout(
-    sidebarPanel(
-      selectInput("year", "Select Year:", 
-        choices = c("All", sort(unique(bills$year))), 
-        selected = "All"),
-      selectInput("status", "Select Status:", 
-        choices = c("All", unique(bills$status)), 
-        selected = "All"),
-      selectInput("state_filter", "Select State:", 
-        choices = c("All", unique_states), # Use the variable from global.r
-        selected = "All")
-    ),
-    mainPanel(
-      tags$div(class = "card", leafletOutput("map", height = 400)),
-      br(),
-      tags$div(class = "card", plotlyOutput("pie", height = 300)),
-      br(),
-      tags$div(class = "card", tableOutput("table"))
+ui <- bslib::page_navbar(
+  # Main site title
+  title = "Youth Digital Policy Repository",
+  # Include CSS in the header
+  header = tags$head(
+    includeCSS("www/styles.css")
+  ),
+
+  bslib::nav_panel(
+    title = "Dashboard", # The title that appears in the navbar tab
+    sidebarLayout(
+      sidebarPanel(
+        selectInput("year", "Select Year:", 
+          choices = c("All", sort(unique(bills$year))), 
+          selected = "All"),
+        selectInput("status", "Select Status:", 
+          choices = c("All", unique(bills$status)), 
+          selected = "All"),
+        selectInput("state_filter", "Select State:", 
+          choices = c("All", unique_states), # Use the variable from global.r
+          selected = "All")
+      ),
+      mainPanel(
+        tags$div(class = "card", leafletOutput("map", height = 400)),
+        br(),
+        tags$div(class = "card", plotlyOutput("pie", height = 300)),
+        br(),
+        tags$div(class = "card", tableOutput("table"))
+      )
     )
+  ),
+  bslib::nav_panel(
+    title = "About"
   )
 )
